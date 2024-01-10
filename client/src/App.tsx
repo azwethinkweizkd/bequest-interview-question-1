a0a7ab63ff847dba7d4aaa5e9890c9182cd0f059
Kevin Devlin <azwethinkweizkd@gmail.com>
2024-01-09 20:14:09 -0700

    FE Verify Data complete
    Verifies the data on the server with a get and response with a message from the back end if the data is verified or not

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,68 +3,82 @@ import React, { useEffect, useState } from "react";
 const API_URL = "http://localhost:8080";
 
 function App() {
-  const [data, setData] = useState<string>();
+	const [data, setData] = useState<string>();
 
-  useEffect(() => {
-    getData();
-  }, []);
+	useEffect(() => {
+		getData();
+	}, []);
 
-  const getData = async () => {
-    const response = await fetch(API_URL);
-    const { data } = await response.json();
-    setData(data);
-  };
+	const getData = async () => {
+		const response = await fetch(API_URL);
+		const { data } = await response.json();
+		setData(data);
+	};
 
-  const updateData = async () => {
-    await fetch(API_URL, {
-      method: "POST",
-      body: JSON.stringify({ data }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
+	const updateData = async () => {
+		await fetch(API_URL, {
+			method: "POST",
+			/**
+			 * * Important, change the id to compromise data. Server has a user id of 1 stored in the db
+			 */
+			body: JSON.stringify({ data, id: 1 }),
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		});
 
-    await getData();
-  };
+		await getData();
+	};
 
-  const verifyData = async () => {
-    throw new Error("Not implemented");
-  };
+	const verifyData = async () => {
+		try {
+			const response = await fetch(`${API_URL}/verify`, {
+				method: "GET",
+			});
 
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        position: "absolute",
-        padding: 0,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        gap: "20px",
-        fontSize: "30px",
-      }}
-    >
-      <div>Saved Data</div>
-      <input
-        style={{ fontSize: "30px" }}
-        type="text"
-        value={data}
-        onChange={(e) => setData(e.target.value)}
-      />
+			const { message } = await response.json();
 
-      <div style={{ display: "flex", gap: "10px" }}>
-        <button style={{ fontSize: "20px" }} onClick={updateData}>
-          Update Data
-        </button>
-        <button style={{ fontSize: "20px" }} onClick={verifyData}>
-          Verify Data
-        </button>
-      </div>
-    </div>
-  );
+			alert(message);
+		} catch (error) {
+			console.error("Error verifying data:", error.message);
+		}
+
+		await getData();
+	};
+
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+				display: "flex",
+				position: "absolute",
+				padding: 0,
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+				gap: "20px",
+				fontSize: "30px",
+			}}>
+			<div>Saved Data</div>
+			<input
+				style={{ fontSize: "30px" }}
+				type="text"
+				value={data}
+				onChange={(e) => setData(e.target.value)}
+			/>
+
+			<div style={{ display: "flex", gap: "10px" }}>
+				<button style={{ fontSize: "20px" }} onClick={updateData}>
+					Update Data
+				</button>
+				<button style={{ fontSize: "20px" }} onClick={verifyData}>
+					Verify Data
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
